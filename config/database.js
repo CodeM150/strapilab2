f83e76ab13a973bb3c0aeac5f1ce3d808a0023b1
CodeWV <49157162+CodeM150@users.noreply.github.com>
2021-12-24 02:21:34 -0300

    1

--- config/database.js
+++ config/database.js
@@ -4,11 +4,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env("DATABASE_HOST", "postgres"),
-      port: env.int("DATABASE_PORT", 15432),
-      database: env("DATABASE_NAME", "datasheets"),
-      user: env("DATABASE_USERNAME", "strapi"),
-      password: env("DATABASE_PASSWORD", "strapi"),
+      host: env("DATABASE_HOST", "strapi-labs2.postgres.database.azure.com"),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "strapidb"),
+      user: env("DATABASE_USERNAME", "strapilabadmin@strapi-labs2"),
+      password: env("DATABASE_PASSWORD", "@Strapilabsadmin"),
       ssl: env.bool("DATABASE_SSL", false),
     },
     useNullAsDefault: true,
